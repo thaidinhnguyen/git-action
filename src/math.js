bfe8d8f2f69de93b0c8870016d4150e431418775
ThaiDinhNguyen_BZ <thai.dinh.nguyen@border-z.co.jp>
2025-07-09 11:38:45 +0700

    fix

--- src/math.js
+++ src/math.js
@@ -1,11 +1,10 @@
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
-
 
 // This function multiplies two numbers
 function multiply(a, b) {
-    return a * b;
+  return a * b + 0;
 }
 
 module.exports = { add, multiply };
